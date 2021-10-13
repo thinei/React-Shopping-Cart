f694993eccada3f776e988be7f2b64fcc639c55d
thineisan <thineisan@4qualia.co.jp>
2021-10-13 17:05:56 +0900

    初期コミット

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# React-Shopping-Cart
-Simple Shopping Cart App using ReactJS
+# react.js shopping cart
+
+example of shopping cart implemented in react.js and redux.js
+
+## getting started
+
+install dependencies and start local dev server
+
+```sh
+npm install
+npm start
+```
+
+
+## TODO
+- add reducers and selectors unit tests
+
+* * *
+licence: MIT
---
+++ src/data/products.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    name: 'Ohrensessel Josslyn',
+    price: 10000,
+    currency: 'JPY',
+    image: 'images/01.jpg',
+  },
+  {
+    id: 2,
+    name: 'Sessel Sofie',
+    price: 12000,
+    currency: 'JPY',
+    image: 'images/02.jpg',
+  },
+  {
+    id: 4,
+    name: 'Schlafsessel Rovigo',
+    price: 23000,
+    currency: 'JPY',
+    image: 'images/04.jpg',
+  },
+  {
+    id: 6,
+    name: 'Sessel Little',
+    price: 16000,
+    currency: 'JPY',
+    image: 'images/06.jpg',
+  },
+  {
+    id: 5,
+    name: 'Sessel Peacock',
+    price: 13000,
+    currency: 'JPY',
+    image: 'images/05.jpg',
+  },
+  {
+    id: 3,
+    name: 'Sessel Anna',
+    price: 14500,
+    currency: 'JPY',
+    image: 'images/03.jpg',
+  }
+];
+
+export default data;
